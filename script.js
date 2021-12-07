2ab200b43b2ac3eeb2ea54a62619f88cf419dadc
bystand <92830997+bystanda@users.noreply.github.com>
2021-12-07 18:15:22 +0000

    Pure HTML done, now for CSS

--- script.js
+++
@@ -1,5 +0,0 @@
-function openWin() {
-  var myWindow = window.open("", "", "width=400, height=200");
-  myWindow.opener.document.getElementById("demo").innerHTML = 
-  "A new window has been opened.";
-}
